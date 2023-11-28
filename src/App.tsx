597805f63e3048c0e2e96dd61a895637d86b5c18
Scoopsies <mooninites103@gmail.com>
2023-11-28 11:29:28 -0600

    fixed TS config and files

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Card from "./components/Card";
+import BeerType from "./Types/BeerType.types";
+
+function App() {
+  const [beers, setBeers] = useState<BeerType[]>([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const {data} = await axios.get(`https://api.punkapi.com/v2/beers?page=1&per_page=10`)
+      setBeers(data)
+    }
+    fetchData()
+  }, [])
+
+  const loadMore = () => {
+    const fetchData = async() => {
+      const {data} = await axios.get(`https://api.punkapi.com/v2/beers?page=${page + 1}&per_page=10`)
+      setBeers([...beers, ...data])
+    }
+    fetchData()
+    setPage(page + 1)
+  }
+
+  console.log(beers)
+
+  return (
+    <div>
+      <h1>D.I.Y. DOG</h1>
+      <div className="recipeContainer">
+        {beers.map(beer => {
+          return (
+            <Card key={beer.id} beer={beer} />
+          )
+        })}
+      </div>
+      <button onClick={() => loadMore()}>Load More</button>
+    </div>
+    
+  )
+}
+
+export default App

9073ba91f565b2ac3b1dc9b0383b233c6286945e
Scoopsies <mooninites103@gmail.com>
2024-01-08 12:31:06 -0600

    more desktop tweaks

--- src/components/Ingredients.tsx
+++ src/components/Ingredients.tsx
@@ -4,63 +4,64 @@ const Ingredients = ({beer} : {beer: BeerType}) => {
 
   return (
     <div className="ingredients">
-        <div className="ingredient">
-          <h4>Malt:</h4>
-          <ul className="malt">
-              <div>
-                {
-                  beer.ingredients.malt.map((_malt, index) => {
-                    return (
-                        <li key={index}>{`
-                        ${_malt.amount.value.toFixed(1)}
-                        ${_malt.amount.unit}
-                        ${_malt.name}`}</li>
-                    )
-                  })
-                }
-              </div>
-            </ul>
-        </div>
-          <div className="ingredient">
-            <h4>Hops:</h4>
-            <ul className="hops">
-              <div>
-                {
-                beer.ingredients.hops.map((hop, index) => {
+      <div className="ingredient">
+        <h4>Malt:</h4>
+        <ul className="malt">
+            <div>
+              {
+                beer.ingredients.malt.map((_malt, index) => {
                   return (
-                    <div key={index}>
-                        <li>{`
-                        ${hop.amount.value.toFixed(1)}
-                        ${hop.amount.unit}
-                        ${hop.name}
-                        @ ${hop.add} ${hop.add === 'dry hop' ? '' : 'of boil'}
-                        (for ${hop.attribute}${hop.attribute === 'aroma' ? '' : 'ing' })`}</li>
-                    </div>
+                      <li key={index}>{`
+                      ${_malt.amount.value.toFixed(1)}
+                      ${_malt.amount.unit}
+                      ${_malt.name}`}</li>
                   )
-                })}
-              </div>
-            </ul>
+                })
+              }
+            </div>
+          </ul>
+      </div>
+      
+      <div className="ingredient">
+        <h4>Hops:</h4>
+        <ul className="hops">
+          <div>
+            {
+            beer.ingredients.hops.map((hop, index) => {
+              return (
+                <div key={index}>
+                    <li>{`
+                    ${hop.amount.value.toFixed(1)}
+                    ${hop.amount.unit}
+                    ${hop.name}
+                    @ ${hop.add} ${hop.add === 'dry hop' ? '' : 'of boil'}
+                    (for ${hop.attribute}${hop.attribute === 'aroma' ? '' : 'ing' })`}</li>
+                </div>
+              )
+            })}
           </div>
+        </ul>
+      </div>
 
       <div className="ingredient">
-      <h4>Yeast:</h4>
-      <ul className='yeast'>
-        <li>{beer.ingredients.yeast}</li>
-      </ul>
-      {
-      beer.method.twist ? (
-      <ul className="twist">
-        <h4>Twist:</h4>
+        <h4>Yeast:</h4>
+        <ul className='yeast'>
+          <li>{beer.ingredients.yeast}</li>
+        </ul>
         {
-          beer.method.twist.split(',').map((_twist, index) => {
-            return (
-              <li key={index}>{_twist}</li>
-            )
-          })
+        beer.method.twist ? (
+        <ul className="twist">
+          <h4>Twist:</h4>
+          {
+            beer.method.twist.split(',').map((_twist, index) => {
+              return (
+                <li key={index}>{_twist}</li>
+              )
+            })
+          }
+        </ul>)
+        : null
         }
-      </ul>)
-      : null
-      }
       </div>
   </div>
   )

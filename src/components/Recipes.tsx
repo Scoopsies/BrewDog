76e1f1b6cb54f164624a51f0023bc51055792222
Scoopsies <mooninites103@gmail.com>
2024-04-15 15:45:32 -0500

    trying console logging import.meta in production

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -10,7 +10,7 @@ import { useLocation } from "react-router-dom";
 
 const Recipes = () => {
   const {search} = useLocation()
-  const isDevMode = import.meta.env.DEV
+  console.log(import.meta)
 
   const fetchData = async ({
     pageParam,
@@ -20,9 +20,9 @@ const Recipes = () => {
   }): Promise<any> => {
     console.log('fetching data')
     const { data } = await axios.get(
-      isDevMode ? `http://localhost:8080/beers` : `https://brewdog-api.onrender.com/?page=${pageParam}`
+      // isDevMode ? `http://localhost:8080/beers` : `https://brewdog-api.onrender.com/?page=${pageParam}`
       // `http://localhost:8080/beers` //For dev
-      // `https://brewdog-api.onrender.com/?page=${pageParam}` //For production
+      `https://brewdog-api.onrender.com/?page=${pageParam}` //For production
       // `https://api.punkapi.com/v2/beers?page=${pageParam}&${search.slice(1)}`
     );
     return data;

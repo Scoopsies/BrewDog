cd4efbe3e07cfa5ab77cab567141b7d9cf112d7a
Scoopsies <mooninites103@gmail.com>
2024-04-16 12:16:24 -0500

    put key in correct place for mapping food pairings

--- src/components/FoodPairings.tsx
+++ src/components/FoodPairings.tsx
@@ -5,10 +5,10 @@ const FoodPairings = ({beer}: {beer: BeerType}) => {
     <ul>
         <h2 className="title">Food Pairings</h2>
         <div className='food pairings'>
-            {beer.food_pairing.map(food => {
+            {beer.food_pairing.map((food, index) => {
                 return (
-                  <div>
-                    <div key={food}>{food}</div>
+                  <div key={index}>
+                    <div>{food}</div>
                   </div>
                 )
             })}
